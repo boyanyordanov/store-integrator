482e5e3a96fdad1b722605dc3af65b788e2e06a7
Boyan Yordanov <b.yordanov@cloudcart.com>
2015-10-16 10:35:09 +0300

    Adds first tests and xml stubs for the ebay sdk integration

---
+++ tests/eBay/SdkWrappingTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boyan
+ * Date: 16.10.15
+ * Time: 08:36
+ */
+
+namespace StoreIntegrator\tests\eBay;
+
+
+class SdkWrappingTest extends \PHPUnit_Framework_TestCase
+{
+
+}
