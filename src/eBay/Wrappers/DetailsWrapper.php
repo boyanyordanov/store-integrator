dfb140b2e97d4da45d2fd717b317f67d17d63c72
Boyan Yordanov <b.yordanov@cloudcart.com>
2015-11-12 14:36:06 +0200

    Adds methods to retrieve shipping locations.

--- src/eBay/Wrappers/DetailsWrapper.php
+++ src/eBay/Wrappers/DetailsWrapper.php
@@ -4,6 +4,7 @@ namespace StoreIntegrator\eBay\Wrappers;
 
 use DTS\eBaySDK\Trading\Enums\DetailNameCodeType;
 use DTS\eBaySDK\Trading\Types\GeteBayDetailsRequestType;
+use DTS\eBaySDK\Trading\Types\GetUserRequestType;
 use StoreIntegrator\eBay\EbayShippingService;
 
 /**
@@ -17,12 +18,7 @@ class DetailsWrapper extends EbayWrapper
      */
     public function getShippingMethods()
     {
-        $request = new GeteBayDetailsRequestType();
-        $request->DetailName = [DetailNameCodeType::C_SHIPPING_SERVICE_DETAILS];
-
-        $this->addAuthToRequest($request);
-
-        $response = $this->service->geteBayDetails($request);
+        $response = $this->getEbayDetail(DetailNameCodeType::C_SHIPPING_SERVICE_DETAILS);
 
         $result = [];
 
@@ -31,5 +27,37 @@ class DetailsWrapper extends EbayWrapper
         }
 
         return $result;
+    }
+
+    /**
+     * @return \DTS\eBaySDK\Trading\Types\GeteBayDetailsResponseType
+     */
+    public function getShippingLocations()
+    {
+        return $this->getEbayDetail(DetailNameCodeType::C_SHIPPING_LOCATION_DETAILS);
+    }
+
+    /**
+     * @return \DTS\eBaySDK\Trading\Types\GeteBayDetailsResponseType
+     */
+    public function getShippingExcludeLocations()
+    {
+        return $this->getEbayDetail(DetailNameCodeType::C_EXCLUDE_SHIPPING_LOCATION_DETAILS);
+    }
+
+    /**
+     * @param $detail
+     * @return \DTS\eBaySDK\Trading\Types\GeteBayDetailsResponseType
+     * @throws \StoreIntegrator\Exceptions\MissingTokenException
+     */
+    private function getEbayDetail($detail)
+    {
+        $request = new GeteBayDetailsRequestType();
+        $request->DetailName = [$detail];
+
+        $this->addAuthToRequest($request);
+
+        $response = $this->service->geteBayDetails($request);
+        return $response;
     }
 }
